d4a4580174a66bafd1a99a86e4d3cf4c90d68cba
Gabriel Lee <chihonglee777@gmail.com>
2019-04-01 16:26:06 +0100

    more beatification done

--- missile-command.c
+++ missile-command.c
@@ -2,58 +2,61 @@
 #include <string.h>
 #include <stdio.h>
 
-#define FRAMEWIDTH 125
-#define FRAMEHEIGHT 45
+#define FRAME_WIDTH 125
+#define FRAME_HEIGHT 45
+
+#define TITLE_PADDING_HORIZONTAL 30
+#define TITLE_PADDING_VERTICAL 12
 
 int main() {
     initscr();
     raw();
 
-    WINDOW *startScreen = newwin(FRAMEHEIGHT, FRAMEWIDTH, 0, 0);
+    WINDOW *start_screen = newwin(FRAME_HEIGHT, FRAME_WIDTH, 0, 0);
     curs_set(FALSE);
     start_color();
     init_pair(1, COLOR_RED, COLOR_WHITE);
-    wattron(startScreen, COLOR_PAIR(1));
+    wattron(start_screen, COLOR_PAIR(1));
     FILE *fp = fopen("graphics/missile-command-text", "r");
     char symbol;
     int y, x;
-    for (int i = 0; i < 12; i++) {
-        getyx(startScreen, y, x);
-        mvwaddch(startScreen, y, FRAMEWIDTH - 2, ACS_VLINE);
-        wmove(startScreen, y + 1, 0);
+    for (int i = 0; i < TITLE_PADDING_VERTICAL; i++) {
+        getyx(start_screen, y, x);
+        mvwaddch(start_screen, y, FRAME_WIDTH - 2, ACS_VLINE);
+        wmove(start_screen, y + 1, 0);
     }
-    getyx(startScreen, y, x);
-    wmove(startScreen, y, x + 30);
+    getyx(start_screen, y, x);
+    wmove(start_screen, y, x + TITLE_PADDING_HORIZONTAL);
     while ((symbol = getc(fp)) != EOF) {
         if (symbol == '#') {
-            waddch(startScreen, ACS_CKBOARD);
+            waddch(start_screen, ACS_CKBOARD);
         } else if (symbol == '\n') {
-            getyx(startScreen, y, x);
-            mvwaddch(startScreen, y, FRAMEWIDTH - 2, ACS_VLINE);
-            wrefresh(startScreen);
-            getyx(startScreen, y, x);
-            wmove(startScreen, y + 1, 30);
+            getyx(start_screen, y, x);
+            mvwaddch(start_screen, y, FRAME_WIDTH - 2, ACS_VLINE);
+            wrefresh(start_screen);
+            getyx(start_screen, y, x);
+            wmove(start_screen, y + 1, TITLE_PADDING_HORIZONTAL);
         } else if (symbol == ' ') {
-            getyx(startScreen, y, x);
-            wmove(startScreen, y, x + 1);
+            getyx(start_screen, y, x);
+            wmove(start_screen, y, x + 1);
         } else {
             perror("Unexpected character in missile-command-text");
         }
-        wrefresh(startScreen);
+        wrefresh(start_screen);
     }
-    for (int i = 0; i < 12; i++) {
-        getyx(startScreen, y, x);
-        mvwaddch(startScreen, y, FRAMEWIDTH - 2, ACS_VLINE);
-        wmove(startScreen, y + 1, 0);
+    for (int i = 0; i < TITLE_PADDING_VERTICAL; i++) {
+        getyx(start_screen, y, x);
+        mvwaddch(start_screen, y, FRAME_WIDTH - 2, ACS_VLINE);
+        wmove(start_screen, y + 1, 0);
     }
-    for (int i = 0; i < FRAMEWIDTH - 2; i++) {
-        waddch(startScreen, ACS_HLINE);
-        wrefresh(startScreen);
+    for (int i = 0; i < FRAME_WIDTH - 2; i++) {
+        waddch(start_screen, ACS_HLINE);
+        wrefresh(start_screen);
     }
-    waddch(startScreen, ACS_LRCORNER);
-    wrefresh(startScreen);
+    waddch(start_screen, ACS_LRCORNER);
+    wrefresh(start_screen);
     fclose(fp);
-    wattroff(startScreen, COLOR_PAIR(1));
-    wgetch(startScreen);
+    wattroff(start_screen, COLOR_PAIR(1));
+    wgetch(start_screen);
     endwin();
 }
