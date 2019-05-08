14ddb6c0c22526894afdbf2ca909ebb6775da456
Gabriel Lee <chihonglee777@gmail.com>
2019-05-08 01:32:41 +0100

    clean up and add comments to highscore.c and .h

--- highscore.c
+++ highscore.c
@@ -1,10 +1,6 @@
 #include "highscore.h"
 #include "functions.h"
 
-#include "threading/list.h"
-#include "threading/thread.h"
-#include "threading/input.h"
-#include "threading/scheduler.h"
 #include "threading/sleeper.h"
 
 #include <ncurses.h>
@@ -12,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+// draws the background with ground, cities and scores
 void draw_highscore_background(WINDOW *screen) {
     for (int i = 0; i < 6; i++)
         cities[i].live = 1;
@@ -24,7 +21,7 @@ void highscore() {
     wattron(score_screen, A_BOLD);
     wattron(score_screen, COLOR_PAIR(4));
 
-    // read highscore file
+    // read highscore file into entries
     FILE *highscore_file = fopen(".highscore", "r");
     char entry[12];
     char entries[8][2][6];
@@ -36,6 +33,7 @@ void highscore() {
 
     draw_highscore_background(score_screen);
 
+    // get player initial
     mvwprintw(score_screen, 18, FRAME_WIDTH / 2 - 5, "GREAT SCORE");
     mvwprintw(score_screen, 20, FRAME_WIDTH / 2 - 9, "ENTER YOUR INITIALS");
     wmove(score_screen, 16, FRAME_WIDTH / 2 - 1);
@@ -53,24 +51,30 @@ void highscore() {
     draw_highscore_background(score_screen);
     mvwprintw(score_screen, 12, FRAME_WIDTH / 2 - 5, "HIGH SCORES");
 
-    int rank = -1;
-    for (int i = 7; i > 0; i--) { // PROBLEM!!!!
+    // add user record into table if it gets into the first 8
+
+    // retermine the rank
+    int rank = -1; // TODO: not a good way to handle the ranking, the whole part should be redo
+    for (int i = 7; i > 0; i--) {
         if (atoi(entries[i][1]) < score)
             continue;
         rank = i;
         break;
     }
 
+    // shift records to the end
     for (int i = 7; i > rank + 1; i--) {
         strcpy(entries[i][0], entries[i - 1][0]);
         strcpy(entries[i][1], entries[i - 1][1]);
     }
 
+    // add new record
     strcpy(entries[rank + 1][0], initial);
     char score_str[6];
     sprintf(score_str, "%i", score);
     strcpy(entries[rank + 1][1], score_str);
 
+    // display high score table
     wattron(score_screen, COLOR_PAIR(4));
     for (int i = 0; i < 8; i++) {
         mvwprintw(score_screen, i + 14, FRAME_WIDTH / 2 - 6, entries[i][0]);
@@ -78,6 +82,7 @@ void highscore() {
     }
     wrefresh(score_screen);
 
+    // write result back to highscore file
     highscore_file = fopen(".highscore", "w");
     if (highscore_file) {
         for (int i = 0; i < 8; i++)
--- highscore.h
+++ highscore.h
@@ -1,6 +1,9 @@
 #ifndef HIGHSCORE_H
 #define HIGHSCORE_H
 
+/**
+* Executes the highscore stage of the game.
+*/
 void highscore();
 
 #endif
